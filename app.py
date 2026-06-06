65a7150c75c2395f114339f0314aeeb9236e42ec
Akshita Sanvi <akstansanvi1905@gmail.com>
2026-06-06 16:38:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load('LogReg_heart_model.pkl')
+scaler = joblib.load('heart_scaler.pkl')
+expected_columns = joblib.load('heart_columns.pkl')
+
+st.title("Heart Stroke Prediction by Akshita❤️")
+st.markdown("Provide the following details to predict the likelihood of a heart stroke:")
+
+age = st.slider("Age", 18, 100, 40)
+sex = st.selectbox("Sex", ["Male", "Female"])
+pain = st.selectbox("Chest Pain Type", ["ATA", "NAP", "TA", "ASY"])
+resting_bp = st.number_input("Resting Blood Pressure (mm Hg)", 80, 200, 120)
+cholesterol = st.number_input("Cholesterol (mg/dl)", 100, 600, 200)
+fasting_bs = st.selectbox("Fasting Blood Sugar > 120 mg/dl", [0, 1])
+resting_ecg = st.selectbox("Resting ECG", ["Normal", "ST", "LVH"])
+max_hr = st.number_input("Max Heart Rate Achieved", 60, 220, 150)
+exercise_angina = st.selectbox("Exercise Induced Angina", ["Yes", "No"])
+oldpeak = st.slider("Oldpeak (ST depression)", 0.0, 6.0, 1.0)
+st_slope = st.selectbox("ST Slope", ["Up", "Flat", "Down"])
+
+if st.button("Predict"):
+    raw_input = {
+        "Age": age,
+        "RestingBP": resting_bp,
+        "Cholesterol": cholesterol,
+        "FastingBS": fasting_bs,
+        "MaxHR": max_hr,
+        "Oldpeak": oldpeak,
+        "Sex_" + sex: 1,
+        "ChestPainType_" + pain: 1,
+        "RestingECG_" + resting_ecg: 1,   
+        "ExerciseAngina_" +  exercise_angina: 1,            
+        "ST_Slope_" + st_slope: 1
+    }
+
+    input_df = pd.DataFrame([raw_input])
+
+    for col in expected_columns:
+      if col not in input_df.columns:
+         input_df[col] = 0
+
+    input_df = input_df[expected_columns]
+    #scaled_input = scaler.transform(input_df)        
+
+    prediction = model.predict(input_df)[0]
+
+    if prediction == 1:
+        st.error("⚠️High risk of heart stroke. Please consult a doctor.")
+    else:
+        st.success("✅Low risk of heart stroke. Keep up the healthy lifestyle!")
+
+
